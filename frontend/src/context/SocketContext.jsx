dbd5086f3d4d8ede9e3f02b4fc3e58b4f5b2bca5
shubham <shubmittal.92@gmail.com>
2024-07-23 20:11:45 +0530

    socket io updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https:/localhost:5000", {
+			const socket = io("https://chit-chat-app-1.onrender.com/", {
 				query: {
 					userId: authUser._id,
 				},
